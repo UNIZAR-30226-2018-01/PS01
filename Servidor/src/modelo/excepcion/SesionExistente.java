084fc70776e6b98d68f88c72009956750849eb9a
alberto <697589@unizar.es>
2018-03-28 21:53:11 +0200

    Cambio de tipo de proyecto
    Debido a problemas con la creación del fichero web.xml y de creación del fichero WAR para el despliegue, se ha cambioado el tipo de proyecto de Eclipse y borrado la versión que daba problemas como los mencionados anteriormente. La hoja Excel llamada Servidor recoge las tareas realizadas en la parte del servidor.

--- Servidor/src/modelo/excepcion/SesionExistente.java
+++
@@ -1,9 +0,0 @@
-package modelo.excepcion;
-
-public class SesionExistente extends ExcepcionBase{
-	private static final long serialVersionUID = 1L;
-	
-	public SesionExistente(String info) {
-		super(info);
-	}
-}
